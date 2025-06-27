855c8bc52553b24e383d0392be0dad61a68d3c0f
Mile1971 <milejovancevic888@gmail.com>
2025-06-27 11:38:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+
+def scalping_bot():
+    print("Scalping bot pokrenut!")
+    # Ovde dodajemo osnovnu logiku
+    while True:
+        print("Proveravam tržište...")
+        # Ovde ćeš dodati pozive ka Bybit API-ju
+        time.sleep(60)  # pauza 60 sekundi između ciklusa
+
+scalping_bot()
